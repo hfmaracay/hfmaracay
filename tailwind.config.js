3257a9e970a0b07e368d073b8c21abff373a0cfc
Dayan Betancourt <dkbetancourt@gmail.com>
2022-09-26 16:29:31 -0400

    style :art: Template Landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,26 @@ module.exports = {
             fontFamily: {
                 sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
             },
+            screens: {
+                sm: "540px",
+                // => @media (min-width: 576px) { ... }
+          
+                md: "720px",
+                // => @media (min-width: 768px) { ... }
+          
+                lg: "960px",
+                // => @media (min-width: 992px) { ... }
+          
+                xl: "1140px",
+                // => @media (min-width: 1200px) { ... }
+          
+                "2xl": "1320px",
+                // => @media (min-width: 1400px) { ... }
+            },
+            container: {
+                center: true,
+                padding: "16px",
+            },
             colors: {
                 'hfred': '#ff6967',
                 'hfgray': {
@@ -25,8 +45,21 @@ module.exports = {
                     DEFAULT: '#60a5fa',
                     light: '#bde6f4',
                     dark: '#387bd9',
-                }
-            }
+                },
+                black: "#212b36",
+                dark: "#090E34",
+                "dark-700": "#090e34b3",
+                primary: "#3056D3",
+                secondary: "#13C296",
+                "body-color": "#637381",
+                warning: "#FBBF24",
+            },
+            boxShadow: {
+                input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
+                pricing: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
+                "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
+                testimonial: "0px 60px 120px -20px #EBEFFD",
+            },
         },
     },
 
